3246968debf7bae67125438520af3e25e6f34ce0
AstralArchitect <matthieu.jlp.poncet@gmail.com>
2024-01-16 21:29:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -39,6 +39,7 @@ try:
                 sense.show_message(extinction_message, text_colour=(127, 0, 127), scroll_speed=0.1)
                 sleep(1)  # Attendre une seconde pour éviter une fermeture accidentelle
                 os.system("sudo git clone https://github.com/AstralArchitect/python-sense-hat.git /root/python/update/")
+                sleep(4)
                 os.system("sudo mv /root/python/update/* /root/python/")
                 sleep(10)
                 os.system("sudo reboot")
