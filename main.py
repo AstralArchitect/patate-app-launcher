45e1472d0d9a5d4ce13fe271eaf3f0cb140bb441
AstralArchitect <matthieu.jlp.poncet@gmail.com>
2024-01-18 20:01:06 +0100

    update

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ try:
                 ennemiy = [3, 4]
                 mx = 0
                 my = 0
-                temps = 1
+                temps = 1.5
                 sleep(1)
                 a = 0
                 sense.set_pixel(youx, youy[0], 127, 127, 0)
@@ -167,7 +167,7 @@ try:
                         break
                     if bally == 6 and bally == (ennemiy[1] or ennemiy[0]):
                         my = 1
-                    if bally == 6 and not bally == (ennemiy[1] or ennemiy[0]):
+                    if temps < 0.3 or bally == 6 and not bally == (ennemiy[1] or ennemiy[0]):
                         my = 1
                         message = "Vous avez gagné !"
                         sense.show_message(message, text_colour=(70, 127, 70), scroll_speed=0.1)
@@ -175,7 +175,7 @@ try:
                         break
                     elif bally == 1:
                         my = 0
-                    temps = temps - 0.01
+                    temps = temps - 0.005
                     for event in events:
                         if event.action == "pressed" and event.direction == "up":
                             if not (youy[1] < 2):
