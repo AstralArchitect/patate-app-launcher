8028b9fd4972e81f1e96b5a25ba95213c2ee1c6d
AstralArchitect <matthieu.jlp.poncet@gmail.com>
2024-01-18 08:44:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,8 @@ try:
                 extinction_message = "Mise a jour..."
                 sense.show_message(extinction_message, text_colour=(127, 0, 127), scroll_speed=0.1)
                 sleep(1)  # Attendre une seconde pour éviter une fermeture accidentelle
-
+                
+                os.system("sudo rm -r /root/python/update/.git /root/python/update/*")
                 sense.set_pixel(0, 3, 127, 127, 0)
 
                 sense.set_pixel(0, 4, 127, 127, 0)
@@ -51,28 +52,27 @@ try:
                 sense.set_pixel(1, 3, 127, 127, 0)
                 
                 sense.set_pixel(1, 4, 127, 127, 0)
-                
-                os.system("sudo rm -r /root/python/update/.git /root/python/update/*")
                 os.system("sudo git clone https://github.com/AstralArchitect/python-sense-hat.git /root/python/update/")
-                os.system("sudo mv /root/python/update/main.py /root/python/")
-                os.system("sudo rm -r /root/python/update/.git /root/python/update/README.md")
+                
                 sense.set_pixel(2, 3, 127, 127, 0)
 
                 sense.set_pixel(2, 4, 127, 127, 0)
 
-                os.system("sudo apt update -y")
                 sense.set_pixel(3, 3, 127, 127, 0)
 
                 sense.set_pixel(3, 4, 127, 127, 0)
                 
+                os.system("sudo mv /root/python/update/main.py /root/python/")
+
                 sense.set_pixel(4, 3, 127, 127, 0)
 
                 sense.set_pixel(4, 4, 127, 127, 0)
-                os.system("sudo apt upgrade -y")
 
                 sense.set_pixel(5, 3, 127, 127, 0)
 
                 sense.set_pixel(5, 4, 127, 127, 0)
+
+                os.system("sudo rm -r /root/python/update/.git /root/python/update/README.md")
 
                 sense.set_pixel(6, 3, 127, 127, 0)
 
