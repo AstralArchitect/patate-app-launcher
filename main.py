c3e82bdac51b620c98d19169f0244c8bcbe97aba
AstralArchitect <matthieu.jlp.poncet@gmail.com>
2024-01-18 09:21:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ try:
                 message = f'CPU:{cpu_usage:.1f}%, Mem:{memory_usage:.1f}%'
                 sense.show_message(message, text_colour=(0, 127, 0), scroll_speed=0.1)
             elif event.action == "pressed" and event.direction == "up":
-                date_actuelle = datetime.now()
+                date_actuelle = datetime.datetime.now()
                 date_formatee = date_actuelle.strftime("%d/%m/%Y %H:%M")
                 sense.show_message(date_formatee, text_colour=(127, 127, 0), scroll_speed=0.1)
             elif event.action == "pressed" and event.direction == "down":
