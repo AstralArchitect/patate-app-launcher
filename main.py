644fc783e0449103d7f4d4e980e940a0c7a08a44
AstralArchitect <matthieu.jlp.poncet@gmail.com>
2024-01-20 14:05:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -133,6 +133,25 @@ try:
                     sense.set_pixel(ennemix, ennemiy[0], 0, 0, 0)
                     sense.set_pixel(ennemix, ennemiy[1], 0, 0, 0)
                     events = sense.stick.get_events()
+                    for event in events:
+                        if event.action == "pressed" and event.direction == "up":
+                            if not (youy[1] < 2):
+                                sense.set_pixel(youx, youy[0], 0, 0, 0)
+                                sense.set_pixel(youx, youy[1], 0, 0, 0)
+                                sense.set_pixel(0, 0, 0, 127, 127)
+                                sense.set_pixel(0, 7, 0, 127, 127)
+                                youy[0] = youy[0] - 1
+                                youy[1] = youy[1] - 1
+                                afficher()
+                        elif event.action == "pressed" and event.direction == "down":
+                            if not (youy[1] > 6):
+                                sense.set_pixel(youx, youy[0], 0, 0, 0)
+                                sense.set_pixel(youx, youy[1], 0, 0, 0)
+                                sense.set_pixel(0, 0, 0, 127, 127)
+                                sense.set_pixel(0, 7, 0, 127, 127)
+                                youy[0] = youy[0] + 1
+                                youy[1] = youy[1] + 1
+                                afficher()
                     if mx == 0:
                         ballx = ballx + 1
                     elif mx == 1:
