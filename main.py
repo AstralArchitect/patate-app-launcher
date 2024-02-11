d2297f5ee05a3ac099c5faf1ea137d64d0687a8b
AstralArchitect <154975712+AstralArchitect@users.noreply.github.com>
2024-02-11 17:51:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,56 +10,6 @@ sense = SenseHat()
 
 try:
     sense.clear()
-    #mise a jour du programme en allant chercher sur github
-    os.system("sudo rm -r /root/python/update/*")
-    sense.set_pixel(0, 3, 127, 127, 0)
-
-    sense.set_pixel(0, 4, 127, 127, 0)
-                
-    os.system("sudo git clone https://github.com/AstralArchitect/python-sense-hat.git /root/python/update/")
-    f = open("/root/python/version.txt")
-    f2 = open("/root/python/update/version.txt")
-    v = f.read()
-    vn = f2.read()
-    b = 1
-    if v == vn:
-        b = 0
-    if b == 1:
-        os.system("sudo rm -r /root/python/update/.git")
-        sense.set_pixel(1, 3, 127, 127, 0)
-                
-        sense.set_pixel(1, 4, 127, 127, 0)
-        
-        sense.set_pixel(2, 3, 127, 127, 0)
-
-        sense.set_pixel(2, 4, 127, 127, 0)
-
-        sense.set_pixel(3, 3, 127, 127, 0)
-
-        sense.set_pixel(3, 4, 127, 127, 0)
-                
-        os.system("sudo mv /root/python/update/main.py /root/python/")
-        os.system("sudo mv /root/python/update/version.txt /root/python/")
-
-        sense.set_pixel(4, 3, 127, 127, 0)
-
-        sense.set_pixel(4, 4, 127, 127, 0)
-
-        sense.set_pixel(5, 3, 127, 127, 0)
-
-        sense.set_pixel(5, 4, 127, 127, 0)
-
-        os.system("sudo rm -r /root/python/update/.git /root/python/update/README.md /root/python/update/installation/")
-
-        sense.set_pixel(6, 3, 127, 127, 0)
-
-        sense.set_pixel(6, 4, 127, 127, 0)
-
-        sense.set_pixel(7, 3, 127, 127, 0)
-
-        sense.set_pixel(7, 4, 127, 127, 0)
-        sleep(1)
-        sys.exit()
     welcome_message = "Bonjour."
     sense.show_message(welcome_message, text_colour=(0, 0, 127), scroll_speed=0.1)
 
