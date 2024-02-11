344c31dff06fb876b0f795cf1aa60fe1ae2da538
AstralArchitect <154975712+AstralArchitect@users.noreply.github.com>
2024-02-11 18:18:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -153,31 +153,18 @@ try:
                 os.system("sudo shutdown now")
             # Mettre à jour le programme
             elif event.action == "pressed" and event.direction == "middle":
-                
-                os.system("sudo rm -r /root/python/update/.git /root/python/update/*")
                 sense.set_pixel(0, 3, 127, 127, 0)
 
                 sense.set_pixel(0, 4, 127, 127, 0)
                 
-                os.system("git clone https://github.com/AstralArchitect/python-sense-hat /root/python/update/")
-                f = open("/root/python/version.txt")
-                f2 = open("/root/python/update/version.txt")
-                v = f.read()
-                vn = f2.read()
-
-                if v == vn:
-                    message = "redemmarrage..."
-                    sense.show_message(message, text_colour=(0, 0, 127), scroll_speed=0.1)
-                    sleep(0.5)
-                    sys.exit()
-                else:
-                    message = "Mise a jour..."
-                    sense.show_message(message, text_colour=(127, 0, 127), scroll_speed=0.1)
+                message = "Mise a jour..."
+                
+                sense.show_message(message, text_colour=(127, 0, 127), scroll_speed=0.1)
+                
                 sense.set_pixel(0, 3, 127, 127, 0)
 
                 sense.set_pixel(0, 4, 127, 127, 0)
-                os.system("sudo rm -r /root/python/update/.git /root/python/update/version.txt")
-                os.system("sudo git clone https://github.com/AstralArchitect/python-sense-hat.git /root/python/update/")
+                os.system("git pull")
                 sleep(0.1)
                 
                 sense.set_pixel(1, 3, 127, 127, 0)
@@ -194,8 +181,6 @@ try:
 
                 sense.set_pixel(3, 4, 127, 127, 0)
                 
-                os.system("sudo mv /root/python/update/main.py /root/python/")
-                os.system("sudo mv /root/python/update/version.txt /root/python/")
                 sleep(0.1)
                 sense.set_pixel(4, 3, 127, 127, 0)
 
@@ -205,7 +190,7 @@ try:
 
                 sense.set_pixel(5, 4, 127, 127, 0)
 
-                os.system("sudo rm -r /root/python/update/.git /root/python/update/README.md /root/python/update/installation/")
+                os.system("sudo rm -r /root/python/update/README.md /root/python/installation/")
                 sleep(0.1)
                 sense.set_pixel(6, 3, 127, 127, 0)
 
