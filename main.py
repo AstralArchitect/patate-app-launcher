2c495b09a0964470271bd0f14134555b7813da87
AstralArchitect <matthieu.jlp.poncet@gmail.com>
2024-02-13 11:27:22 +0100

    update

--- main.py
+++ main.py
@@ -83,6 +83,7 @@ try:
                 sense.show_message(programmes[nombre], text_colour=(0, 0, 127), scroll_speed=0.1)
                 fin = False
                 while fin == False:
+                    events = sense.stick.get_events()
                     for event in events:
                         if event.action == "pressed" and event.direction == "middle":
                             up = open("/root/python-config/" + programmes[nombre])
