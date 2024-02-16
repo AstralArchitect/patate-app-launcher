d5202951170bd1cfbe9c96c522fbb1d69630db05
AstralArchitect <matthieu.jlp.poncet@gmail.com>
2024-02-16 20:15:46 +0100

    update

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ try:
                     elif event.action == "pressed" and event.direction == "left":
                         up = open("/root/python-config/" + programmes[programme])
                         loc = up.read()
-                        uninstall = "desintallation..."
+                        uninstall = "desinstallation..."
                         sense.show_message(uninstall, text_colour=(127, 0, 0), scroll_speed=0.1)
                         os.system("rm -r /root/python-config/" + programmes[programme] +  " " + loc)
                         os._exit(0)
