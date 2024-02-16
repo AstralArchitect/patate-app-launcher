c6ff4d93c107f65807d7c37f94e893e768bb7f7f
AstralArchitect <matthieu.jlp.poncet@gmail.com>
2024-02-16 20:08:14 +0100

    update

--- main.py
+++ main.py
@@ -74,6 +74,12 @@ try:
                             exec(f.read())
                         up.seek(0)
                         up.close()
+                    elif event.action == "pressed" and event.direction == "left":
+                        up = open("/root/python-config/" + programmes[programme])
+                        loc = up.read()
+                        uninstall = "desintallation..."
+                        sense.show_message(uninstall, text_colour=(0, 0, 127), scroll_speed=0.1)
+                        os.system("rm -r /root/python-packages/" + programmes[programme] +  "/ " + loc)
                 if programme == len(programmes) - 1:
                     programme = 0
                     continue
